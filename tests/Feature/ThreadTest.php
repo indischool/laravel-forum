3f1dec4fedeca9729555aa2fe12d256d4b360f2f
jdssem <jdssem@gmail.com>
2019-02-22 13:31:28 +0900

    2. Test-Driving Threads

---
+++ tests/Feature/ThreadTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThreadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_browse_all_threads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+
+        $response->assertSee($thread->title);
+    }
+}
